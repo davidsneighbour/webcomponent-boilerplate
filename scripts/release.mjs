734c7db90b2853b033ba32c80eea7ba013a86b69
Patrick Kollitsch <patrick@davids-neighbour.com>
2025-01-04 07:03:09 +0700

    build(refactor): cleanup release script
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,18 +1,6 @@
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
-// import fs from 'node:fs/promises';
-
 const execAsync = promisify(exec);
-
-// async function tagExists(tagName) {
-//   try {
-//     const { stdout } = await execAsync('git tag');
-//     const tags = stdout.split('\n').map((tag) => tag.trim());
-//     return tags.includes(tagName);
-//   } catch {
-//     return false;
-//   }
-// }
 
 async function release() {
   try {
@@ -23,18 +11,10 @@ async function release() {
     await execAsync('npm test');
 
     console.log('Bumping version...');
-    const { stdout: newVersionOutput } = await execAsync('npm version patch'); // Bumps the version
-    const newVersion = newVersionOutput.trim(); // Extracts the new version tag (e.g., "v1.0.2")
+    const { stdout: newVersionOutput } = await execAsync('npm version patch');
+    const newVersion = newVersionOutput.trim();
 
     const tagName = newVersion.startsWith('v') ? newVersion : `v${newVersion}`;
-
-    // console.log(`Checking if tag ${tagName} exists...`);
-    // if (!(await tagExists(tagName))) {
-    //   console.log(`Creating git tag for version ${tagName}...`);
-    //   await execAsync(`git tag ${tagName}`);
-    // } else {
-    //   console.log(`Tag ${tagName} already exists. Skipping tag creation.`);
-    // }
 
     console.log('Pushing changes and tags...');
     await execAsync('git push && git push --tags');
